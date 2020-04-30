e611a82d2558235e12058ec37f1a33e9be6784f3
kazu-megane <kazukazu.music1214@gmail.com>
2020-05-01 01:35:05 +0900

    [fix]エラーになっていた部分を修正

--- components/pc/template/index.stories.tsx
+++ components/pc/template/index.stories.tsx
@@ -8,41 +8,81 @@ const imageStub: Pick<Props, "images"> = {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
   ],
 };
