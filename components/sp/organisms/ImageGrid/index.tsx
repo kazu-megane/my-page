0f6f6a7938aadee9284112409627e5658e8d04ef
kazu-megane <kazukazu.music1214@gmail.com>
2020-09-29 00:58:21 +0900

    [fix]scroll処理

--- components/sp/organisms/ImageGrid/index.tsx
+++ components/sp/organisms/ImageGrid/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect, Fragment } from "react";
 import Binder from "../../../all/atoms/helpers/Binder";
 import style from "./index.module.scss";
 
@@ -14,29 +14,52 @@ export interface Props {
   onClick?: () => void;
 }
 
-const ImageGrid: FC<Props> = ({ className, images, hasNext, onClick }) =>
-  images.length > 0 ? (
+const ImageGrid: FC<Props> = ({ className, images, hasNext, onClick }) => {
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const bookmark = document.getElementById(`page-${page - 1}`);
+    if (page >= 2 && bookmark) {
+      bookmark.scrollIntoView({ block: "center" });
+    }
+  }, [images]);
+
+  return images.length > 0 ? (
     <Binder classNames={[style.ImageGrid, className]}>
       <div>
         <ul className={style.ImageGrid__items}>
-          {images.map((image, index) => (
-            <li className={style.ImageGrid__item} key={index}>
-              <p className={style.ImageGrid__image}>
-                <img
-                  className={style.ImageGrid__imageContent}
-                  src={image.url}
-                  alt={image.alt}
-                  width="512"
-                  height="341"
-                  loading="lazy"
-                />
-              </p>
-            </li>
-          ))}
+          {images.map((image, index) => {
+            return (
+              <li
+                id={index !== 0 && index % 32 === 0 ? `page-${index / 32}` : ""}
+                className={style.ImageGrid__item}
+                key={index}
+              >
+                <p className={style.ImageGrid__image}>
+                  <img
+                    className={style.ImageGrid__imageContent}
+                    src={image.url}
+                    alt={image.alt}
+                    width="512"
+                    height="341"
+                    loading="lazy"
+                  />
+                </p>
+              </li>
+            );
+          })}
         </ul>
         {hasNext ? (
           <div className={style.ImageGrid__actionArea}>
-            <button onClick={onClick} className={style.ImageGrid__button}>
+            <button
+              onClick={() => {
+                if (onClick) {
+                  setPage(page + 1);
+                  onClick();
+                }
+              }}
+              className={style.ImageGrid__button}
+            >
               M O R E
             </button>
           </div>
@@ -44,5 +67,6 @@ const ImageGrid: FC<Props> = ({ className, images, hasNext, onClick }) =>
       </div>
     </Binder>
   ) : null;
+};
 
 export default ImageGrid;
