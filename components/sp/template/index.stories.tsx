6f5df51bfe791b9b28ac4f9af49ed8885b72e782
kazu-megane <kazukazu.music1214@gmail.com>
2020-10-03 23:35:14 +0900

    [fix]stub

--- components/sp/template/index.stories.tsx
+++ components/sp/template/index.stories.tsx
@@ -9,41 +9,61 @@ const imageStub: Pick<Props, "images" | "columnNum"> = {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
     {
       url:
         "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
       alt: "",
-    },
-    {
-      url:
-        "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
-      alt: "",
-    },
-    {
-      url:
-        "https://pbs.twimg.com/media/EMxv6eMUcAAsdeQ?format=jpg&name=4096x4096",
-      alt: "",
+      data: {
+        width: "",
+        height: "",
+        creationTime: "",
+      },
     },
   ],
   columnNum: 4,
