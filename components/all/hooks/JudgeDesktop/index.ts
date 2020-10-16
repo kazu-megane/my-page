aefe2d826b25502a6dd0bf4cba144defb8580fe3
kazu-megane <kazukazu.music1214@gmail.com>
2020-10-17 00:19:33 +0900

    [fix]レイアウト崩れを修正

--- components/all/hooks/JudgeDesktop/index.ts
+++ components/all/hooks/JudgeDesktop/index.ts
@@ -3,12 +3,8 @@ import { SP_WIDTH } from "~/constants";
 
 export const useJudgeDesktop = (isPc: boolean) => {
   const [isDesktop, setIsDesktop] = useState(isPc);
-  let currentWidth = 0;
 
   function judgeDevice() {
-    if (currentWidth === window.innerWidth) {
-      return;
-    }
     if (window.innerWidth <= SP_WIDTH) {
       setIsDesktop(false);
     } else {
@@ -18,7 +14,6 @@ export const useJudgeDesktop = (isPc: boolean) => {
 
   useEffect(() => {
     if (window) {
-      currentWidth = window.innerWidth;
       judgeDevice();
       window.addEventListener("resize", judgeDevice);
     }
