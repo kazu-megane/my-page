130f52bce2910f6146b26e35e264e4c7979faafc
kazu-megane <kazukazu.music1214@gmail.com>
2020-10-26 00:26:36 +0900

    [fix]画像サイズを入れる

--- components/pc/organisms/WorkContent/index.tsx
+++ components/pc/organisms/WorkContent/index.tsx
@@ -12,7 +12,7 @@ const WorkContent: FC<Props> = ({ className }) => (
       <ul className={style.WorkContent__items}>
         <li className={style.WorkContent__item}>
           <a href="https://design-ship.jp/" target="_blank" rel="noopener noreferrer" className={style.WorkContent__image}>
-            <img src="/work/designship.png" className={style.WorkContent__imageContent} />
+            <img src="/work/designship.png" width="340" height="180" className={style.WorkContent__imageContent} />
           </a>
           <p className={style.WorkContent__year}>2020</p>
           <p className={style.WorkContent__title}>Designship</p>
--- components/sp/organisms/WorkContent/index.tsx
+++ components/sp/organisms/WorkContent/index.tsx
@@ -12,7 +12,7 @@ const WorkContent: FC<Props> = ({ className }) => (
       <ul className={style.WorkContent__items}>
         <li className={style.WorkContent__item}>
           <a href="https://design-ship.jp/" className={style.WorkContent__itemImage}>
-            <img src="/work/designship.png" className={style.WorkContent__imageContent} />
+            <img src="/work/designship.png" width="330" height="175" className={style.WorkContent__imageContent} />
           </a>
           <p className={style.WorkContent__year}>2020</p>
           <p className={style.WorkContent__title}>Designship</p>
