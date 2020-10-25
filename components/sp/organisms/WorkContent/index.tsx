c607215c5287ceb3cfbac51be9ee002bba2bd008
kazu-megane <kazukazu.music1214@gmail.com>
2020-10-25 19:39:04 +0900

    [add]work

--- components/sp/organisms/WorkContent/index.tsx
+++ components/sp/organisms/WorkContent/index.tsx
@@ -9,7 +9,15 @@ type Props = {
 const WorkContent: FC<Props> = ({ className }) => (
   <Binder classNames={[style.WorkContent, className]}>
     <div>
-      <p className={style.WorkContent__text}>Coming soon.</p>
+      <ul className={style.WorkContent__items}>
+        <li className={style.WorkContent__item}>
+          <a href="https://design-ship.jp/" className={style.WorkContent__itemImage}>
+            <img src="/work/designship.png" className={style.WorkContent__imageContent} />
+          </a>
+          <p className={style.WorkContent__year}>2020</p>
+          <p className={style.WorkContent__title}>Designship</p>
+        </li>
+      </ul>
     </div>
   </Binder>
 )
