26cc361efdd4226b10a3ae6c9d48a4a8e6901771
kazu-megane <kazukazu.music1214@gmail.com>
2020-10-28 13:34:28 +0900

    [fix]videoListにskeltonを追加

--- components/pc/organisms/VideoList/index.tsx
+++ components/pc/organisms/VideoList/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import Binder from '~/components/all/atoms/helpers/Binder';
 import style from "./index.module.scss";
 
@@ -16,68 +16,74 @@ type Props = {
   className?: string;
 }
 
+const Video: FC<{ src: string }> = ({ src }) => {
+  const [isDoneLoad, setIsDoneLoad] = useState(false);
+
+  return (
+    <Binder
+      classNames={[
+        style.VideoList__video,
+        isDoneLoad ? style["VideoList__video--done"] : "",
+      ]}
+    >
+      <p>
+        <span className={style.VideoList__videoSkelton} />
+        <iframe src={src} className={style.VideoList__videoContent} onLoad={() => {
+          setIsDoneLoad(true);
+        }} />
+      </p>
+    </Binder>
+  );
+}
+
 const VideoList: FC<Props> = ({ className }) => (
   <Binder classNames={[style.VideoList, className]}>
     <div>
       <ul className={style.VideoList__items}>
         <li className={style.VideoList__item}>
-          <div className={style.VideoList__video}>
-            <iframe src={`https://www.youtube.com/embed/${VIDEO.AOMORI}?playsinline=1`} className={style.VideoList__videoContent} />
-          </div>
+          <Video src={`https://www.youtube.com/embed/${VIDEO.AOMORI}?playsinline=1`} />
           <div className={style.VideoList__description}>
             <p className={style.VideoList__descriptionHeading}>Aomori Vlog</p>
             <p className={style.VideoList__descriptionDate}>2020.10.10-2020.10.12</p>
           </div>
         </li>
         <li className={style.VideoList__item}>
-          <div className={style.VideoList__video}>
-            <iframe src={`https://www.youtube.com/embed/${VIDEO.SNOWBOARD}?playsinline=1`} className={style.VideoList__videoContent} />
-          </div>
+          <Video src={`https://www.youtube.com/embed/${VIDEO.SNOWBOARD}?playsinline=1`} />
           <div className={style.VideoList__description}>
             <p className={style.VideoList__descriptionHeading}>Snowboard Vlog</p>
             <p className={style.VideoList__descriptionDate}>2020.01.17-2020.01.20</p>
           </div>
         </li>
         <li className={style.VideoList__item}>
-          <div className={style.VideoList__video}>
-            <iframe src={`https://www.youtube.com/embed/${VIDEO.FINLAND_3}?playsinline=1`} className={style.VideoList__videoContent} />
-          </div>
+          <Video src={`https://www.youtube.com/embed/${VIDEO.FINLAND_3}?playsinline=1`} />
           <div className={style.VideoList__description}>
             <p className={style.VideoList__descriptionHeading}>Finland Vlog 3</p>
             <p className={style.VideoList__descriptionDate}>2019.12.06-2019.12.07</p>
           </div>
         </li>
         <li className={style.VideoList__item}>
-          <div className={style.VideoList__video}>
-            <iframe src={`https://www.youtube.com/embed/${VIDEO.FINLAND_2}?playsinline=1`} className={style.VideoList__videoContent} />
-          </div>
+          <Video src={`https://www.youtube.com/embed/${VIDEO.FINLAND_2}?playsinline=1`} />
           <div className={style.VideoList__description}>
             <p className={style.VideoList__descriptionHeading}>Finland Vlog 2</p>
             <p className={style.VideoList__descriptionDate}>2019.12.05-2019.12.06</p>
           </div>
         </li>
         <li className={style.VideoList__item}>
-          <div className={style.VideoList__video}>
-            <iframe src={`https://www.youtube.com/embed/${VIDEO.FINLAND_1}?playsinline=1`} className={style.VideoList__videoContent} />
-          </div>
+          <Video src={`https://www.youtube.com/embed/${VIDEO.FINLAND_1}?playsinline=1`} />
           <div className={style.VideoList__description}>
             <p className={style.VideoList__descriptionHeading}>Finland Vlog 1</p>
             <p className={style.VideoList__descriptionDate}>2019.12.01-2019.12.05</p>
           </div>
         </li>
         <li className={style.VideoList__item}>
-          <div className={style.VideoList__video}>
-            <iframe src={`https://www.youtube.com/embed/${VIDEO.GERMANY}?playsinline=1`} className={style.VideoList__videoContent} />
-          </div>
+          <Video src={`https://www.youtube.com/embed/${VIDEO.GERMANY}?playsinline=1`} />
           <div className={style.VideoList__description}>
             <p className={style.VideoList__descriptionHeading}>Germany Vlog</p>
             <p className={style.VideoList__descriptionDate}>2019.09.15-2019.12.20</p>
           </div>
         </li>
         <li className={style.VideoList__item}>
-          <div className={style.VideoList__video}>
-            <iframe src={`https://www.youtube.com/embed/${VIDEO.AIZU}?playsinline=1`} className={style.VideoList__videoContent} />
-          </div>
+          <Video src={`https://www.youtube.com/embed/${VIDEO.AIZU}?playsinline=1`} />
           <div className={style.VideoList__description}>
             <p className={style.VideoList__descriptionHeading}>AizuKomagadake Vlog</p>
             <p className={style.VideoList__descriptionDate}>2019.08.17-2019.08.18</p>
