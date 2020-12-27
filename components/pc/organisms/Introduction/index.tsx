ae8ed44dcf0db6822ef1cc493a071036198e7b28
kazu-megane <kazukazu.music1214@gmail.com>
2020-12-27 22:17:49 +0900

    [fix]aboutを更新

--- components/pc/organisms/Introduction/index.tsx
+++ components/pc/organisms/Introduction/index.tsx
@@ -9,25 +9,55 @@ type Props = {
 const Introduction: FC<Props> = ({ className }) => (
   <Binder classNames={[style.Introduction, className]}>
     <div>
+      <p>
+        <img
+          src="/myPhoto.png"
+          alt="myPhoto"
+          width="300"
+          height="300"
+          className={style.Introduction__image}
+        />
+      </p>
       <div className={style.Introduction__columns}>
         <div className={style.Introduction__columnLeft}>
-          <img
-            src="/myPhoto.png"
-            alt="myPhoto"
-            width="300"
-            height="300"
-            className={style.Introduction__image}
-          />
-        </div>
-        <div className={style.Introduction__columnRight}>
           <p className={style.Introduction__name}>Kazuya Hashimoto</p>
           <p className={style.Introduction__text}>
-            I was born in Japan in 1994. I'm currently working as front-end enginner and web designer at <a href="https://about.yahoo.co.jp/" target="_blank" rel="noopener noreferrer">Yahoo Japan Corporation</a>.
+            I was born in Japan in 1994. I'm currently working as front-end enginner and web designer based in Tokyo.
           </p>
           <p className={style.Introduction__text}>
             Now, I'm learning photography and videos as a hobby.
           </p>
         </div>
+        <div className={style.Introduction__columnRight}>
+          <p className={style.Introduction__name}>橋本 和也</p>
+          <p className={style.Introduction__text}>1994年生まれ。フロントエンドエンジニア、デザイナー。</p>
+          <p className={style.Introduction__text}>仕事とは関係なく、写真や動画の勉強をしている。</p>
+        </div>
+      </div>
+      <div className={style.Introduction__biography}>
+        <p className={style.Introduction__biographyTitle}>Biography</p>
+        <ul className={style.Introduction__biographyItems}>
+          <li className={style.Introduction__biographyItem}>
+            <p className={style.Introduction__biographyItemHeading}>1994.12</p>
+            <p className={style.Introduction__biographyItemContent}>千葉県船橋市生まれ</p>
+          </li>
+          <li className={style.Introduction__biographyItem}>
+            <p className={style.Introduction__biographyItemHeading}>2014.04</p>
+            <p className={style.Introduction__biographyItemContent}>東京電機大学 未来科学部 情報メディア学科 入学</p>
+          </li>
+          <li className={style.Introduction__biographyItem}>
+            <p className={style.Introduction__biographyItemHeading}>2017.12</p>
+            <p className={style.Introduction__biographyItemContent}>HCG2017シンポジウム 学生優秀インタラクティブ発表賞 受賞</p>
+          </li>
+          <li className={style.Introduction__biographyItem}>
+            <p className={style.Introduction__biographyItemHeading}>2018.03</p>
+            <p className={style.Introduction__biographyItemContent}>東京電機大学 未来科学部 情報メディア学科 卒</p>
+          </li>
+          <li className={style.Introduction__biographyItem}>
+            <p className={style.Introduction__biographyItemHeading}>2018.04</p>
+            <p className={style.Introduction__biographyItemContent}>ヤフー株式会社 入社</p>
+          </li>
+        </ul>
       </div>
     </div>
   </Binder>
--- components/sp/organisms/Introduction/index.tsx
+++ components/sp/organisms/Introduction/index.tsx
@@ -18,13 +18,45 @@ const Introduction: FC<Props> = ({ className }) => (
           className={style.Introduction__imageContent}
         />
       </p>
-      <p className={style.Introduction__name}>Kazuya Hashimoto</p>
-      <p className={style.Introduction__text}>
-        I was born in Japan in 1994. I'm currently working as front-end enginner and web designer at <a href="https://about.yahoo.co.jp/">Yahoo Japan Corporation</a>.
-      </p>
-      <p className={style.Introduction__text}>
-        Now, I'm learning photography and videos as a hobby.
-      </p>
+      <div className={style.Introduction__profile}>
+        <p className={style.Introduction__profileName}>Kazuya Hashimoto</p>
+        <p className={style.Introduction__profileText}>
+          I was born in Japan in 1994. I'm currently working as front-end enginner and web designer based in Tokyo.
+        </p>
+        <p className={style.Introduction__profileText}>
+          Now, I'm learning photography and videos as a hobby.
+        </p>
+      </div>
+      <div className={style.Introduction__profile}>
+        <p className={style.Introduction__profileName}>橋本 和也</p>
+        <p className={style.Introduction__profileText}>1994年生まれ。フロントエンドエンジニア、デザイナー。</p>
+        <p className={style.Introduction__profileText}>仕事とは関係なく、写真や動画の勉強をしている。</p>
+      </div>
+      <div className={style.Introduction__biography}>
+        <p className={style.Introduction__biographyTitle}>Biography</p>
+        <ul className={style.Introduction__biographyItems}>
+          <li className={style.Introduction__biographyItem}>
+            <p className={style.Introduction__biographyItemHeading}>1994.12</p>
+            <p className={style.Introduction__biographyItemContent}>千葉県船橋市生まれ</p>
+          </li>
+          <li className={style.Introduction__biographyItem}>
+            <p className={style.Introduction__biographyItemHeading}>2014.04</p>
+            <p className={style.Introduction__biographyItemContent}>東京電機大学 未来科学部 情報メディア学科 入学</p>
+          </li>
+          <li className={style.Introduction__biographyItem}>
+            <p className={style.Introduction__biographyItemHeading}>2017.12</p>
+            <p className={style.Introduction__biographyItemContent}>HCG2017シンポジウム 学生優秀インタラクティブ発表賞 受賞</p>
+          </li>
+          <li className={style.Introduction__biographyItem}>
+            <p className={style.Introduction__biographyItemHeading}>2018.03</p>
+            <p className={style.Introduction__biographyItemContent}>東京電機大学 未来科学部 情報メディア学科 卒</p>
+          </li>
+          <li className={style.Introduction__biographyItem}>
+            <p className={style.Introduction__biographyItemHeading}>2018.04</p>
+            <p className={style.Introduction__biographyItemContent}>ヤフー株式会社 入社</p>
+          </li>
+        </ul>
+      </div>
     </div>
   </Binder>
 );
