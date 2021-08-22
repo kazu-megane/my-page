3c96afdfd0e453159a9b7b44b3997f581b86c9e2
kazu-megane <kazukazu.music1214@gmail.com>
2021-08-23 01:06:00 +0900

    [fix]profile

--- components/sp/organisms/Introduction/index.tsx
+++ components/sp/organisms/Introduction/index.tsx
@@ -11,7 +11,7 @@ const Introduction: FC<Props> = ({ className }) => (
     <div>
       <p className={style.Introduction__image}>
         <img
-          src="/myPhoto2.JPG"
+          src="/myPhoto3.JPG"
           alt="myPhoto"
           width="327"
           height="327"
@@ -21,43 +21,61 @@ const Introduction: FC<Props> = ({ className }) => (
       <div className={style.Introduction__profile}>
         <p className={style.Introduction__profileName}>Kazuya Hashimoto</p>
         <p className={style.Introduction__profileText}>
-          I was born in Japan in 1994. I'm currently working as front-end enginner and web designer based in Tokyo.
+          I was born in Japan in 1994. I'm currently working as front-end
+          enginner and web designer based in Tokyo.
         </p>
         <p className={style.Introduction__profileText}>
-          Now, I'm learning photography and videos as a hobby. Mainly for snaps, landscape photography and portrait photography.
+          Now, I'm learning photography and videos as a hobby. Mainly for snaps,
+          landscape photography and portrait photography.
         </p>
       </div>
       <div className={style.Introduction__profile}>
         <p className={style.Introduction__profileName}>橋本 和也</p>
-        <p className={style.Introduction__profileText}>1994年生まれ。フロントエンドエンジニア、デザイナー。</p>
-        <p className={style.Introduction__profileText}>趣味で写真や動画の勉強をしている。主に、スナップや風景写真、ポートレートの撮影をしている。</p>
+        <p className={style.Introduction__profileText}>
+          1994年生まれ。フロントエンドエンジニア、デザイナー。
+        </p>
+        <p className={style.Introduction__profileText}>
+          趣味で写真や動画の勉強をしている。主に、スナップや風景写真、ポートレートの撮影をしている。
+        </p>
       </div>
       <div className={style.Introduction__biography}>
         <p className={style.Introduction__biographyTitle}>Biography</p>
         <ul className={style.Introduction__biographyItems}>
           <li className={style.Introduction__biographyItem}>
             <p className={style.Introduction__biographyItemHeading}>1994.12</p>
-            <p className={style.Introduction__biographyItemContent}>千葉県船橋市生まれ</p>
+            <p className={style.Introduction__biographyItemContent}>
+              千葉県船橋市生まれ
+            </p>
           </li>
           <li className={style.Introduction__biographyItem}>
             <p className={style.Introduction__biographyItemHeading}>2014.04</p>
-            <p className={style.Introduction__biographyItemContent}>東京電機大学 未来科学部 情報メディア学科 入学</p>
+            <p className={style.Introduction__biographyItemContent}>
+              東京電機大学 未来科学部 情報メディア学科 入学
+            </p>
           </li>
           <li className={style.Introduction__biographyItem}>
             <p className={style.Introduction__biographyItemHeading}>2017.12</p>
-            <p className={style.Introduction__biographyItemContent}>HCG2017シンポジウム 学生優秀インタラクティブ発表賞 受賞</p>
+            <p className={style.Introduction__biographyItemContent}>
+              HCG2017シンポジウム 学生優秀インタラクティブ発表賞 受賞
+            </p>
           </li>
           <li className={style.Introduction__biographyItem}>
             <p className={style.Introduction__biographyItemHeading}>2018.03</p>
-            <p className={style.Introduction__biographyItemContent}>東京電機大学 未来科学部 情報メディア学科 卒</p>
+            <p className={style.Introduction__biographyItemContent}>
+              東京電機大学 未来科学部 情報メディア学科 卒
+            </p>
           </li>
           <li className={style.Introduction__biographyItem}>
             <p className={style.Introduction__biographyItemHeading}>2018.04</p>
-            <p className={style.Introduction__biographyItemContent}>ヤフー株式会社 入社</p>
+            <p className={style.Introduction__biographyItemContent}>
+              ヤフー株式会社 入社
+            </p>
           </li>
           <li className={style.Introduction__biographyItem}>
             <p className={style.Introduction__biographyItemHeading}>2020.10</p>
-            <p className={style.Introduction__biographyItemContent}>Designship 2020 運営</p>
+            <p className={style.Introduction__biographyItemContent}>
+              Designship 2020 運営
+            </p>
           </li>
         </ul>
       </div>
