bf8353ffb322bd60addaab61e01091585d33ae66
kazu-megane <kazukazu.music1214@gmail.com>
2022-01-22 17:10:02 +0900

    [fix]node-fetchをアップデート

--- lib/logics/fetcher/index.ts
+++ lib/logics/fetcher/index.ts
@@ -1,6 +1,9 @@
 import fetch from "node-fetch";
 
-export async function fetcher(url: string, option?: Record<string, any>) {
+export async function fetcher(
+  url: string,
+  option?: Record<string, any>
+): Promise<any> {
   return (await fetch(url, option)).json().catch((error) => {
     throw new Error(error);
   });
