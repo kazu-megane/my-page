3c0a00987f6bb781507580e651799f7d7f5151b9
kazu-megane <kazukazu.music1214@gmail.com>
2022-01-22 23:05:28 +0900

    [fix]node-fetch

--- lib/logics/fetcher/index.ts
+++ lib/logics/fetcher/index.ts
@@ -1,9 +1,6 @@
 import fetch from "node-fetch";
 
-export async function fetcher(
-  url: string,
-  option?: Record<string, any>
-): Promise<any> {
+export async function fetcher(url: string, option?: Record<string, any>) {
   return (await fetch(url, option)).json().catch((error) => {
     throw new Error(error);
   });
