f67b25a841bf91f9f02356f755cdf292c4bc4471
kazu-megane <kazukazu.music1214@gmail.com>
2022-01-25 02:14:12 +0900

    [fix]余白を調整

--- components/pc/organisms/WorkDetail/Designship/index.tsx
+++ components/pc/organisms/WorkDetail/Designship/index.tsx
@@ -1,71 +1,155 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 import style from "./index.module.scss";
 
 const Designship: FC = () => (
   <div className={style.Designship}>
     <p className={style.Designship__image}>
-      <img src="/work/designship.png" alt="designship" width="1200" height="630" className={style.Designship__imageContent} />
+      <img
+        src="/work/designship.png"
+        alt="designship"
+        width="1200"
+        height="630"
+        className={style.Designship__imageContent}
+      />
     </p>
     <p className={style.Designship__title}>Designship</p>
     <p className={style.Designship__year}>2020</p>
-    <p className={style.Designship__role}>Developed a live site and designed a novelty.</p>
+    <p className={style.Designship__role}>
+      Developed a live site and designed a novelty.
+    </p>
     <p className={style.Designship__link}>
-      <a href="https://design-ship.jp/" target="_blank" rel="noopener noreferrer" className={style.Designship__linkContent}>Official website</a>
+      <a
+        href="https://design-ship.jp/2020"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={style.Designship__linkContent}
+      >
+        Official website
+      </a>
     </p>
     <div className={style.Designship__description}>
-      <p className={style.Designship__descriptionTitle}>物語の力で「デザイン」の壁を越える。</p>
+      <p className={style.Designship__descriptionTitle}>
+        物語の力で「デザイン」の壁を越える。
+      </p>
       <p className={style.Designship__descriptionTheme}>
-        <span className={style.Designship__descriptionThemeText}>Chronicle</span>
-        <span className={style.Designship__descriptionThemeText}>デザインという営みに対して、一人ひとりが残してきた異なる軌跡。</span>
-        <span className={style.Designship__descriptionThemeText}> きれいで、整った、ひとつの跡が長く続いていくのではなく、その一瞬、その年、その時代の短い跡が多くの人によって刻まれてひとつの流れが形作られていく。<br />この流れは私たちが受け継ぎ、自分自身で紡ぎつづけるものだ。</span>
-        <span className={style.Designship__descriptionThemeText}>きれいで、整った、ひとつの跡が長く続いていくのではなく、その一瞬、その年、その時代の短い跡が多くの人によって刻まれてひとつの流れが形作られていく。この流れは私たちが受け継ぎ、自分自身で紡ぎつづけるものだ。激しくて大きな変化を伴う社会の中に今、私たちはいる。その中でデザインを通じて、全ての人に希望を与える流れを紡いでいけるだろうか。後に振り返ったときに、Designship2020は大きな流れの転換点だったと言えることを願って。</span>
+        <span className={style.Designship__descriptionThemeTitle}>
+          Chronicle
+        </span>
+        <span className={style.Designship__descriptionThemeTexts}>
+          <span className={style.Designship__descriptionThemeText}>
+            デザインという営みに対して、一人ひとりが残してきた異なる軌跡。
+          </span>
+          <span className={style.Designship__descriptionThemeText}>
+            {" "}
+            きれいで、整った、ひとつの跡が長く続いていくのではなく、その一瞬、その年、その時代の短い跡が多くの人によって刻まれてひとつの流れが形作られていく。
+            <br />
+            この流れは私たちが受け継ぎ、自分自身で紡ぎつづけるものだ。
+          </span>
+          <span className={style.Designship__descriptionThemeText}>
+            きれいで、整った、ひとつの跡が長く続いていくのではなく、その一瞬、その年、その時代の短い跡が多くの人によって刻まれてひとつの流れが形作られていく。この流れは私たちが受け継ぎ、自分自身で紡ぎつづけるものだ。激しくて大きな変化を伴う社会の中に今、私たちはいる。その中でデザインを通じて、全ての人に希望を与える流れを紡いでいけるだろうか。後に振り返ったときに、Designship2020は大きな流れの転換点だったと言えることを願って。
+          </span>
+        </span>
       </p>
       <div className={style.Designship__noveltyDescription}>
-        <p className={style.Designship__noveltyDescriptionTitle}>オンライン開催に馴染むグッズ</p>
+        <p className={style.Designship__noveltyDescriptionTitle}>
+          オンライン開催に馴染むグッズ
+        </p>
         <ul className={style.Designship__noveltyDescriptionItems}>
           <li className={style.Designship__noveltyDescriptionItem}>
             <p className={style.Designship__noveltyItemHeading}>エコバッグ</p>
-            <p className={style.Designship__noveltyItemText}>レジ袋有料化に伴い需要が増しているエコバック。サスティナブルな求められる社会に寄り添うグッズ。</p>
+            <p className={style.Designship__noveltyItemText}>
+              レジ袋有料化に伴い需要が増しているエコバック。サスティナブルな求められる社会に寄り添うグッズ。
+            </p>
           </li>
           <li className={style.Designship__noveltyDescriptionItem}>
             <p className={style.Designship__noveltyItemHeading}>チョコレート</p>
-            <p className={style.Designship__noveltyItemText}>小腹が空いた時や休憩時間のおともになるように、糖分補給のチョコレート。</p>
+            <p className={style.Designship__noveltyItemText}>
+              小腹が空いた時や休憩時間のおともになるように、糖分補給のチョコレート。
+            </p>
           </li>
           <li className={style.Designship__noveltyDescriptionItem}>
-            <p className={style.Designship__noveltyItemHeading}>マイクロファイバークロス</p>
-            <p className={style.Designship__noveltyItemText}>オンライン配信ということで、パソコンやスマートフォン、タブレットを通してDesignshipに参加するため、画面の汚れが気になった時に、さっと使えるクロス。</p>
+            <p className={style.Designship__noveltyItemHeading}>
+              マイクロファイバークロス
+            </p>
+            <p className={style.Designship__noveltyItemText}>
+              オンライン配信ということで、パソコンやスマートフォン、タブレットを通してDesignshipに参加するため、画面の汚れが気になった時に、さっと使えるクロス。
+            </p>
           </li>
         </ul>
       </div>
       <ul className={style.Designship__noveltyImages}>
         <li className={style.Designship__noveltyImage}>
-          <img src="https://pbs.twimg.com/media/Ek6CMhuUcAArVSc?format=jpg&name=large" alt="package" width="625" height="420" className={style.Designship__noveltyImageContent} />
+          <img
+            src="https://pbs.twimg.com/media/Ek6CMhuUcAArVSc?format=jpg&name=large"
+            alt="package"
+            width="625"
+            height="420"
+            className={style.Designship__noveltyImageContent}
+          />
         </li>
         <li className={style.Designship__noveltyImage}>
-          <img src="https://pbs.twimg.com/media/Ek6CMhvVcAAQMn7?format=jpg&name=large" alt="label" width="625" height="420" className={style.Designship__noveltyImageContent} />
+          <img
+            src="https://pbs.twimg.com/media/Ek6CMhvVcAAQMn7?format=jpg&name=large"
+            alt="label"
+            width="625"
+            height="420"
+            className={style.Designship__noveltyImageContent}
+          />
         </li>
         <li className={style.Designship__noveltyImage}>
-          <img src="https://pbs.twimg.com/media/Ek6CMilVkAEqbNy?format=jpg&name=large" alt="allNovelty" width="625" height="420" className={style.Designship__noveltyImageContent} />
+          <img
+            src="https://pbs.twimg.com/media/Ek6CMilVkAEqbNy?format=jpg&name=large"
+            alt="allNovelty"
+            width="625"
+            height="420"
+            className={style.Designship__noveltyImageContent}
+          />
         </li>
         <li className={style.Designship__noveltyImage}>
-          <img src="https://pbs.twimg.com/media/Ek6CMjMU8AA8bAz?format=jpg&name=large" alt="allNovelty" width="625" height="420" className={style.Designship__noveltyImageContent} />
+          <img
+            src="https://pbs.twimg.com/media/Ek6CMjMU8AA8bAz?format=jpg&name=large"
+            alt="allNovelty"
+            width="625"
+            height="420"
+            className={style.Designship__noveltyImageContent}
+          />
         </li>
       </ul>
       <div className={style.Designship__siteDescription}>
-        <p className={style.Designship__siteDescriptionTitle}>オンラインでも会場にいるかのような雰囲気作り</p>
+        <p className={style.Designship__siteDescriptionTitle}>
+          オンラインでも会場にいるかのような雰囲気作り
+        </p>
         <ul className={style.Designship__siteDescriptionItems}>
-          <li className={style.Designship__siteDescriptionItem}>会場の暗転を背景色の変化で再現</li>
-          <li className={style.Designship__siteDescriptionItem}>チャットを通じて参加者同士の交流を促進</li>
-          <li className={style.Designship__siteDescriptionItem}>動画に集中したい人向けのシアターモード</li>
+          <li className={style.Designship__siteDescriptionItem}>
+            会場の暗転を背景色の変化で再現
+          </li>
+          <li className={style.Designship__siteDescriptionItem}>
+            チャットを通じて参加者同士の交流を促進
+          </li>
+          <li className={style.Designship__siteDescriptionItem}>
+            動画に集中したい人向けのシアターモード
+          </li>
           <li className={style.Designship__siteDescriptionItem}>字幕機能</li>
         </ul>
       </div>
       <ul className={style.Designship__siteImages}>
         <li className={style.Designship__siteImage}>
-          <img src="/work/designship/onAir.png" alt="end" width="625" height="370" className={style.Designship__siteImageContent} />
+          <img
+            src="/work/designship/onAir.png"
+            alt="end"
+            width="625"
+            height="370"
+            className={style.Designship__siteImageContent}
+          />
         </li>
         <li className={style.Designship__siteImage}>
-          <img src="/work/designship/end.png" alt="end" width="625" height="370" className={style.Designship__siteImageContent} />
+          <img
+            src="/work/designship/end.png"
+            alt="end"
+            width="625"
+            height="370"
+            className={style.Designship__siteImageContent}
+          />
         </li>
       </ul>
     </div>
@@ -73,4 +157,3 @@ const Designship: FC = () => (
 );
 
 export default Designship;
-
--- components/sp/organisms/WorkDetail/index.tsx
+++ components/sp/organisms/WorkDetail/index.tsx
@@ -1,19 +1,21 @@
-import React, { FC } from 'react';
-import Binder from '~/components/all/atoms/helpers/Binder';
-import Designship from './Designship';
-import PlanckUnits from './PlanckUnits';
+import React, { FC } from "react";
+import Binder from "~/components/all/atoms/helpers/Binder";
+import Designship2021 from "./Designship2021";
+import Designship from "./Designship";
+import PlanckUnits from "./PlanckUnits";
 import style from "./index.module.scss";
 
 type Props = {
   id: string;
   className?: string;
-}
+};
 
 const WorkDetail: FC<Props> = ({ id, className }) => (
   <Binder classNames={[style.WorkDetail, className]}>
     <div>
-      {id === 'Designship' ? <Designship /> : null}
-      {id === 'PlanckUnits' ? <PlanckUnits /> : null}
+      {id === "Designship2021" ? <Designship2021 /> : null}
+      {id === "Designship" ? <Designship /> : null}
+      {id === "PlanckUnits" ? <PlanckUnits /> : null}
     </div>
   </Binder>
 );
